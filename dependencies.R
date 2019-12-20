df1df2324605d9165439785271d5bc11c4bae5b4
Christian Luz <33626459+ceefluz@users.noreply.github.com>
2019-12-20 13:10:47 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,25 +1,7 @@
 # LIST OF REQUIRED PACKAGES -----------------------------------------------
 
 required_packages <- c(
-  "AMR",
-  "data.table",
-  "DT",
-  "ggridges",
-  "lubridate",
-  "plotly",
-  "qicharts2",
-  "rintrojs",
-  "shiny",
-  "shinyBS",
-  "shinycssloaders",
-  "shinydashboard",
-  "shinyjs",
-  "shinyWidgets",
-  "survival",
-  "survminer",
-  "tidyverse",
-  "viridis",
-  "zoo"
+  "checkpoint"
 )
 
 # install missing packages
@@ -29,3 +11,28 @@ new.packages <- required_packages[!(required_packages %in% installed.packages()[
 if (length(new.packages)) {
   install.packages(new.packages)
 }
+
+rm(new.packages)
+
+library(checkpoint)
+checkpoint(snapshotDate ='2019-12-17')
+library(AMR)
+library(data.table)
+library(DT)
+library(ggridges)
+library(lubridate)
+library(plotly)
+library(qicharts2)
+library(rintrojs)
+library(shiny)
+library(shinyBS)
+library(shinycssloaders)
+library(shinydashboard)
+library(shinyjs)
+library(shinyWidgets)
+library(survival)
+library(ggpubr)
+library(survminer)
+library(tidyverse)
+library(viridis)
+library(zoo)
